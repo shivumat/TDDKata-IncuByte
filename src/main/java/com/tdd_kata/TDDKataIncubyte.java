b8d3cdac493cb322781d76834d8ae5eba1537dd2
Shivank Mathur <shivumat@gmail.com>
2021-01-06 22:38:44 +0530

    Task1 empty string implementation done and now test passing

--- src/main/java/com/tdd_kata/TDDKataIncubyte.java
+++ src/main/java/com/tdd_kata/TDDKataIncubyte.java
@@ -1,9 +1,12 @@
 package com.tdd_kata;
 
+import com.tdd_kata.tasks.Task1;
+
 public class TDDKataIncubyte {
 
     public static void main(String[] args) {
-
+        Task1 task1 = new Task1();
+        System.out.print(task1.Add(""));//shall output 0
     }
 
 }
--- src/main/java/com/tdd_kata/tasks/Task1.java
+++ src/main/java/com/tdd_kata/tasks/Task1.java
@@ -2,7 +2,13 @@ package com.tdd_kata.tasks;
 
 public class Task1 {
 
+    public static final String WHITE_SPACE = "\\s";
+    public static final String EMPTY = "";
+
     public int Add(String numbers){
+        if(numbers.replace(WHITE_SPACE, EMPTY).isEmpty()){
+            return 0;
+        }
         return -1;
     }
 }
