2ade2b120d291531bd4d1a509e7543f3497f36a0
Shivank Mathur <shivumat@gmail.com>
2021-01-07 03:09:25 +0530

    Task8 init commit with test cases for all scenarios failing

---
+++ src/main/java/com/tdd_kata/tasks/Task8.java
@@ -0,0 +1,9 @@
+package com.tdd_kata.tasks;
+
+public class Task8 {
+
+    public int Add(String numbersAsString){
+        return -1;
+    }
+
+}
---
+++ src/test/java/com/tdd_kata/tasks/Task8Test.java
@@ -0,0 +1,20 @@
+package com.tdd_kata.tasks;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class Task8Test extends TestCase {
+
+    public void testAdd() {
+
+        //given
+        String numbers = "//[*][;]\n1*2;3*9;5";
+        Task7 task = new Task7();
+
+        //when
+        int result = task.Add(numbers);
+
+        //then
+        Assert.assertEquals(20, result);
+    }
+}
