4178fd61ae8aa24a07e538ccffd06b90219f7590
Shivank Mathur <shivumat@gmail.com>
2021-01-06 22:45:25 +0530

    Task1 refactoring

--- src/main/java/com/tdd_kata/tasks/Task1.java
+++ src/main/java/com/tdd_kata/tasks/Task1.java
@@ -2,13 +2,18 @@ package com.tdd_kata.tasks;
 
 public class Task1 {
 
-    public static final String WHITE_SPACE = "\\s";
+    public static final String WHITE_SPACE = "\\s+";
     public static final String EMPTY = "";
 
     public int Add(String numbers){
-        if(numbers.replace(WHITE_SPACE, EMPTY).isEmpty()){
+        String numbersWithoutSpace = removeWhiteSpace(numbers);
+        if(numbersWithoutSpace.isEmpty()){
             return 0;
         }
         return -1;
+    }
+
+    private String removeWhiteSpace(String string) {
+        return string.replaceAll(WHITE_SPACE, EMPTY);
     }
 }
